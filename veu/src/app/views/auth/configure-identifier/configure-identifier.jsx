9ac0694cb0dfd1ee650822f9f44f41a966cce71f
jprice <jprice@izeni.com>
2021-12-09 16:34:16 -0700

    Use configure-witnesses modal on configure-identifier page, begin advanced options layout

--- veu/src/app/views/auth/configure-identifier/configure-identifier.jsx
+++ veu/src/app/views/auth/configure-identifier/configure-identifier.jsx
@@ -1,24 +1,49 @@
 import m from 'mithril';
-import { Button, Container, TextField } from '../../../components';
+import { Button, Container, Select, TextField } from '../../../components';
+import { ConfigureWitnesses } from '../../../modals';
 import configureIdentifier from '../../../../assets/img/configure-identifier.png';
 
 class ConfigureIdentifier {
-  constructor() {}
+  constructor() {
+    this.showAdvanced = false;
+    this.showConfigureWitnesses = false;
+  }
+
+  toggleAdvanced() {
+    this.showAdvanced = !this.showAdvanced;
+  }
+
+  openConfigureWitnesses() {
+    this.showConfigureWitnesses = true;
+  }
 
   view() {
     return (
       <>
-        <Container class="headspace flex flex-align-center">
+        <ConfigureWitnesses
+          isOpen={this.showConfigureWitnesses}
+          onClose={() => {
+            this.showConfigureWitnesses = false;
+          }}
+        />
+        <Container class="headspace flex">
           <div class="flex-2">
             <img src={configureIdentifier} />
           </div>
-          <div class="flex-3" style="padding: 1rem">
-            <h1>Create Your Alias</h1>
-            <label>What would you like your alias to be?</label>
-            <TextField placeholder="Jane Smith - QVI Corp." />
-            <label>Witnesses</label>
-            <TextField placeholder="GLEIF Witness Network" />
-            <div class="flex flex-justify-end" style={{ marginTop: '2rem' }}>
+          <div class="flex-3" style="padding: 0 1rem">
+            <h1>Configure Identifier</h1>
+            <label class="label">Witnesses</label>
+            <TextField fluid filled placeholder="GLEIF Witness Network" />
+            <div class="flex flex-justify-between" style={{ marginTop: '2rem' }}>
+              <Button
+                class="button__gray"
+                raised
+                iconTrailing={this.showAdvanced ? 'arrow_drop_up' : 'arrow_drop_down'}
+                label="Advanced Options"
+                onclick={() => {
+                  this.toggleAdvanced();
+                }}
+              />
               <Button
                 raised
                 label="Continue"
@@ -27,6 +52,45 @@ class ConfigureIdentifier {
                 }}
               />
             </div>
+            {this.showAdvanced && (
+              <>
+                <div class="flex flex-align-center" style={{ marginTop: '2rem' }}>
+                  <label class="label flex-1">Add/Edit Witnesses</label>
+                  <Button
+                    class="flex-1 button__gray"
+                    raised
+                    label="Add/Edit"
+                    onclick={() => {
+                      this.openConfigureWitnesses();
+                    }}
+                  />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Incepting Signing Threshold</label>
+                  <Select class="flex-1" />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Incepting Key Count</label>
+                  <Select class="flex-1" />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Next Signing Threshold</label>
+                  <Select class="flex-1" />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Next Key Count</label>
+                  <Select class="flex-1" />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Witness Threshold</label>
+                  <Select class="flex-1" />
+                </div>
+                <div class="flex flex-align-center" style={{ marginTop: '1rem' }}>
+                  <label class="label flex-1">Witness Address</label>
+                  <TextField filled initialValue="HTTPS" class="flex-1" />
+                </div>
+              </>
+            )}
           </div>
         </Container>
       </>
