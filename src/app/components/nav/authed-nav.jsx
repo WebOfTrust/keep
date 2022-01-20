ff447d98b696d8644efcddad8a9ac67ad9eec38b
jprice <jprice@izeni.com>
2022-01-20 15:01:16 -0700

    Add temporary github logo user image on authed nav

--- src/app/components/nav/authed-nav.jsx
+++ src/app/components/nav/authed-nav.jsx
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import Container from '../container/container.jsx';
 import IconButton from '../icon-button/icon-button.jsx';
+import githubLogo from '../../../assets/img/github-logo.png';
 import keepLogo from '../../../assets/img/logo/keep.png';
 import './authed-nav.scss';
 
@@ -16,8 +17,9 @@ class AuthedNav {
               </m.route.Link>
             </h2>
             <div class="authedNav__spacer"></div>
-            <IconButton icon="notifications" />
-            <IconButton icon="settings" />
+            <IconButton icon="notifications" style={{ marginRight: '2rem' }} />
+            <IconButton icon="settings" style={{ marginRight: '2rem' }} />
+            <img src={githubLogo} style={{ height: '40px', width: '40px' }} />
           </div>
         </Container>
       </div>
