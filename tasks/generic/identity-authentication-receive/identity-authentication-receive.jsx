89b1822f280e7d26513f20b5bf02fd3a5df679b7
jprice <jprice@izeni.com>
2022-03-03 12:32:30 -0700

    Start wiring up identity authentication-receive task

--- tasks/generic/identity-authentication-receive/identity-authentication-receive.jsx
+++ tasks/generic/identity-authentication-receive/identity-authentication-receive.jsx
@@ -1,5 +1,6 @@
 import m from 'mithril';
 import { Button, TextField, Modal } from '../../../src/app/components';
+import { KERI } from '../../../src/app/services';
 import addNewContacts from '../../../src/assets/img/add-new-contacts.png';
 import responseMessage from '../../../src/assets/img/response-message.png';
 import uploadFile from '../../../src/assets/img/upload-file.png';
@@ -61,18 +62,18 @@ class StepsToAuthenticate {
             this.waitModalOpen = false;
           }}
         />
-        <h3>Identity Authentication of the QAR</h3>
+        <h3>Identity Authentication</h3>
         <p class="p-tag">
-          This module will take you through the steps of how to authenticate QAR Identity. Below are the steps for how
-          to complete the process:
+          This module will take you through the steps of how to authenticate an Identity. Below are the steps for how to
+          complete the process:
         </p>
-        <h3>Steps to Authenticate QAR Identity</h3>
+        <h3>Steps to Authenticate an Identity</h3>
         <ol class="styled-ol" style={{ margin: '2rem 0' }}>
-          <li>Join a Video Call with a QAR</li>
-          <li>Use an OOBI protocol to share your GLEIF Delegated AID with the QAR</li>
-          <li>Send a Challenge Response Message to the QAR.</li>
-          <li>QAR signs and returns Challenge Response Message.</li>
-          <li>GAR verifies signature of the QAR.</li>
+          <li>Both parties Join a Video Call</li>
+          <li>Use an OOBI protocol to accept an AID</li>
+          <li>Send a Challenge Message</li>
+          <li>Other party signs and returns Challenge Message</li>
+          <li>Verify the signature of the other party</li>
         </ol>
         <div class="flex flex-justify-end">
           {/* <Button class="button--gray-dk button--big button--no-transform" raised label="Skip" /> */}
@@ -87,10 +88,10 @@ class JoinVideoCall {
   view(vnode) {
     return (
       <>
-        <h3>Join a Video Call with a QAR</h3>
+        <h3>Join a Video Call with the other party</h3>
         <p class="p-tag">
           In order to start the authentication process, you will need to complete an real-time OOBI session in which you
-          and the QAR are present, You will accept the QAR’s OOBI on a Video Call so that you can receive their
+          and the other party are present, You will accept their OOBI on a Video Call so that you can receive their
           identifying information.
         </p>
         <div class="flex flex-justify-between">
@@ -103,12 +104,31 @@ class JoinVideoCall {
 }
 
 class EnterOOBI {
+  constructor() {}
+
+  oninit() {
+    KERI.listIdentifiers().then((identifiers) => {
+      console.log(identifiers);
+      KERI.getOOBI(identifiers[0].name, 'gar').then((oobi) => {
+        console.log(oobi);
+      });
+    });
+  }
+
   view(vnode) {
     return (
       <>
         <img src={addNewContacts} style={{ width: '40%', margin: '1.5rem 0 0 0' }} />
-        <h3>Accept OOBI</h3>
-        <p class="p-tag">Enter OOBI you received on the Video Call from the QAR below:</p>
+        <h3>
+          Accept <u>OOBI</u>
+        </h3>
+        <p class="p-tag">
+          While on the Video Call, make sure to obtain the other party's <b>URL and OOBI</b>. When you have both for
+          each party, please press continue.
+        </p>
+        <label>AID:</label>
+        <TextField outlined fluid style={{ margin: '0 0 2rem 0' }} />
+        <label>URL:</label>
         <TextField outlined fluid style={{ margin: '0 0 4rem 0' }} />
         <div class="flex flex-justify-between">
           <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
@@ -125,7 +145,7 @@ class GenerateChallengeMessage {
       <>
         <img src={responseMessage} style={{ width: '50%', margin: '1.5rem 0 2rem 0' }} />
         <h3>Generate Challenge Message</h3>
-        <p class="p-tag">The Challenge Response Message generated will be sent to the QVI for verification purposes.</p>
+        <p class="p-tag">The Challenge Message generated will be sent to the other party for verification purposes.</p>
         <div class="flex flex-justify-between">
           <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
           <Button class="button--big button--no-transform" raised label="Continue" onclick={vnode.attrs.continue} />
@@ -141,11 +161,7 @@ class CopyChallengeMessage {
       <>
         <img src={responseMessage} style={{ width: '50%', margin: '1.5rem 0 2rem 0' }} />
         <h3>Copy Challenge Message</h3>
-        <p class="p-tag">
-          Copy the Challenge Message into the chat box while on the Video Call.
-          <br />
-          <br />
-        </p>
+        <p class="p-tag">Copy the Challenge Message into the chat box while on the Video Call.</p>
         <TextField outlined textarea fluid style={{ margin: '0 0 4rem 0' }} />
         <div class="flex flex-justify-between">
           <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
@@ -162,11 +178,7 @@ class ChallengeMessageInProgress {
       <>
         <img src={uploadFile} style={{ width: '60%', margin: '1.5rem 0 2rem 0' }} />
         <h3>Challenge Message in Progress</h3>
-        <p class="p-tag">
-          You will be notified when the QAR signs and returns the Challenge Message.
-          <br />
-          <br />
-        </p>
+        <p class="p-tag">You will be notified when the other party signs and returns the Challenge Message.</p>
         <div class="flex flex-justify-between">
           <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
           <Button class="button--big button--no-transform" raised label="Close" onclick={vnode.attrs.end} />
@@ -178,8 +190,8 @@ class ChallengeMessageInProgress {
 
 class IdentityAuthenticationReceive {
   constructor() {
-    this.currentState = 'steps-to-authenticate';
-    // this.currentState = 'join-video-call';
+    this.currentState = 'enter-oobi';
+    // this.currentState = 'steps-to-authenticate';
   }
 
   view(vnode) {
