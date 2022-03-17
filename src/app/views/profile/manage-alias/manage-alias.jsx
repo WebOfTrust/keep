7db5a7ffa62f980193c62e2c82611423aea25269
natepace <n8wpace@gmail.com>
2022-03-17 17:20:17 -0400

    finished alias manager

--- src/app/views/profile/manage-alias/manage-alias.jsx
+++ src/app/views/profile/manage-alias/manage-alias.jsx
@@ -1,7 +1,7 @@
 import m from 'mithril';
 import githubLogo from '../../../../assets/img/github-logo.png';
 import uploadImage from '../../../../assets/img/upload-image.png';
-import { Button } from '../../../components';
+import { Button, Select } from '../../../components';
 class AliasToEdit {
   view(vnode) {
     return (
@@ -41,8 +41,8 @@ class AliasToEdit {
           </div>
         </div>
         <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button button--no-transform" raised label="New Alias" />
-          <Button class="button button--no-transform" raised label="Edit Alias" onclick={vnode.attrs.continue} />
+          <Button class="button--gray-dk button--big button--no-transform" raised label="New Alias" />
+          <Button class="button--big button--no-transform" raised label="Edit Alias" onclick={vnode.attrs.continue} />
         </div>
       </>
     );
@@ -81,8 +81,8 @@ class AliasToUpdate {
           </div>
         </div>
         <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button button--no-transform" raised label="New Alias" />
-          <Button class="button button--no-transform" raised label="Edit Alias" onclick={vnode.attrs.continue} />
+          <Button class="button--gray-dk button--big button--no-transform" raised label="New Alias" />
+          <Button class="button--big button--no-transform" raised label="Edit Alias" onclick={vnode.attrs.continue} />
         </div>
       </>
     );
@@ -95,7 +95,7 @@ class ReviewAndConfirm {
       <>
         <img src={uploadImage} style={{ width: '50%' }} />
         <h3>Review and Confrim</h3>
-        <div class="flex flex-justify-between" style={{ margin: '2rem 0 2rem 0' }}>
+        <div class="flex flex-justify-between" style={{ margin: '2rem 0 4rem 0' }}>
           <img src={githubLogo} style={{ width: '40%', borderRadius: '50%' }} />
           <div style={{ width: '50%' }}>
             <h4 style={{ color: '#3d63ae' }}>Octocat</h4>
@@ -106,8 +106,93 @@ class ReviewAndConfirm {
         </div>
 
         <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button button--no-transform" raised label="Go Back" />
-          <Button class="button button--no-transform" raised label="Submit" onclick={vnode.attrs.continue} />
+          <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
+          <Button class="button--big button--no-transform" raised label="Submit" onclick={vnode.attrs.continue} />
+        </div>
+      </>
+    );
+  }
+}
+class SelectAliasCred {
+  view(vnode) {
+    return (
+      <>
+        <h1>Profile</h1>
+        <div class="flex flex-justify-between" style={{ margin: '2rem 0 2rem 0' }}>
+          <img src={githubLogo} style={{ width: '40%', borderRadius: '50%' }} />
+          <div style={{ width: '50%' }}>
+            <h4 style={{ color: '#3d63ae' }}>Default Alias: Octocat</h4>
+            <h4 style={{ color: '#737374' }}>
+              <u>Default Credential: External GAR</u>
+            </h4>
+          </div>
+        </div>
+        <h3>Select Default Credential:</h3>
+        <Select options={[{ label: 'External GAR', value: 'External GAR' }]} initialSelection={'External GAR'} />
+        <h3>Edit Alias</h3>
+        <Select options={[{ label: 'Octocat', value: 'Octocat' }]} initialSelection={'Octocat'} />
+
+        <div class="flex flex-justify-between" style={{ margin: '4rem 0 0 0' }}>
+          <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
+          <Button class="button--big button--no-transform" raised label="Submit" onclick={vnode.attrs.continue} />
+        </div>
+      </>
+    );
+  }
+}
+
+class SelectPhoto {
+  view(vnode) {
+    return (
+      <>
+        <img src={uploadImage} style={{ width: '172px' }} />
+        <h3 style={{ margin: '2rem 0' }}>Select a Photo for the Alias</h3>
+        <p class="p-tag" style={{ margin: '2rem 0' }}>
+          If you would like your alias to have a photo instead of the default icon, please upload a photo.
+        </p>
+        <input type="file" style={{ margin: '0 0 10rem 0' }} onchange={vnode.attrs.aliasPhotoChange} />
+        <div class="flex flex-justify-between">
+          <Button
+            class="button--gray-dk button--big button--no-transform"
+            raised
+            label="Go Back"
+            onclick={vnode.attrs.back}
+          />
+          <Button
+            class="button--big button--no-transform"
+            raised
+            label="Continue"
+            // disabled={!vnode.attrs.aliasPhoto}
+            onclick={vnode.attrs.continue}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+class EditProfileAlias {
+  view(vnode) {
+    return (
+      <>
+        <h1>Profile</h1>
+        <div class="flex flex-justify-between" style={{ margin: '2rem 0 2rem 0' }}>
+          <img src={githubLogo} style={{ width: '40%', borderRadius: '50%' }} />
+          <div style={{ width: '50%' }}>
+            <h4 style={{ color: '#3d63ae' }}>Default Alias: Octocat</h4>
+            <h4 style={{ color: '#737374' }}>
+              <u>Default Credential: External GAR</u>
+            </h4>
+          </div>
+        </div>
+        <h3>Default Credential:</h3>
+        <p class="p-tag">External GAR</p>
+        <h3>Edit Alias</h3>
+        <p class="p-tag">Octocat</p>
+
+        <div class="flex flex-justify-between" style={{ margin: '4rem 0 0 0' }}>
+          <Button class="button--gray-dk button--big button--no-transform" raised label="New Alias" />
+          <Button class="button--big button--no-transform" raised label="Edit Alias" onclick={vnode.attrs.continue} />
         </div>
       </>
     );
@@ -136,7 +221,28 @@ class ManageAlias {
             }}
           />
         )}
-        {this.currentState === 'review-and-confirm' && <ReviewAndConfirm />}
+        {this.currentState === 'review-and-confirm' && (
+          <ReviewAndConfirm
+            continue={() => {
+              this.currentState = 'select-alias-cred';
+            }}
+          />
+        )}
+        {this.currentState === 'select-alias-cred' && (
+          <SelectAliasCred
+            continue={() => {
+              this.currentState = 'select-photo';
+            }}
+          />
+        )}
+        {this.currentState === 'select-photo' && (
+          <SelectPhoto
+            continue={() => {
+              this.currentState = 'edit-profile-alias';
+            }}
+          />
+        )}
+        {this.currentState === 'edit-profile-alias' && <EditProfileAlias />}
       </>
     );
   }
