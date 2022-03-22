d2976bd23e84cb935aed03acf28e50a5a907017e
Nate Pace <nate@debian-BULLSEYE-live-builder-AMD64>
2022-03-22 19:41:22 -0400

    added new avr screens

--- tasks/avr/revoke-avr-vlei-credential/revoke-avr-vlei-credential.jsx
+++ tasks/avr/revoke-avr-vlei-credential/revoke-avr-vlei-credential.jsx
@@ -5,20 +5,41 @@ import addNewContacts from '../../../src/assets/img/add-new-contacts.png';
 import verifyCredentials from '../../../src/assets/img/verify-credentials.png';
 class RevokeAVRvLEICredential {
   constructor(vnode) {
+    this.select=false;
     this.step = 0;
 
     this.steps = [
       <>
-        <h3>QAR TASK 6</h3>
-        <img src={approveRequest} style={{ width: '60%', margin: '4rem 0 4rem 4rem' }} />
-        <p class="p-tag">
-          You have now created your GLEIF Delegated AID! While you are waiting for your GLEIF credentials, here is a
-          brief introduction to some of the tasks you can complete in your role.
-          <br />
-          <br />
-        </p>
+        <h3>Request Credential Revocation</h3>
+                <p class="p-tag">
+                Please select the contact and credentials you would like to revoke.</p>
+        
+          <TextField
+            style={{ backgroundColor: 'rgba(0, 0, 0, 0.04)', height: '3rem', margin: '0 0 1.5rem 0', width: '75%' }}
+            filled
+            fluid
+            placeholder="ACME" 
+            // oninput={(e) => {
+            //   this.passcode = e.target.value;
+            // }}
+            iconLeading={{
+              icon: 'search',
+            }}
+          />
+<div>
+  <Card 
+  class='card--active'
+  style={{margin: '2rem 0 2rem 0', height: '6rem'}}>
+    <p>Paul Smith</p>
+    <p>AVR for ACME Corp.</p>
+  </Card>
+  <Card style={{margin: '2rem 0 2rem 0', height: '6rem'}}>
+    <p>Judith Williams</p>
+    <p>AVR for ACME Corp.</p>
+  </Card>
+</div>
         <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button--big button--no-transform" raised label="Skip" />
+          <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
           <Button
             class="button--big button--no-transform"
             raised
@@ -30,41 +51,52 @@ class RevokeAVRvLEICredential {
         </div>
       </>,
       <>
-        <h3>Complete Identity Assurance</h3>
-        <img src={addNewContacts} style={{ width: '50%', margin: '4rem 0 4rem 4rem' }} />
+      <h3>Request Credential Revocation</h3>
+              <p class="p-tag">
+              On submission, a signed request has been sent to the requested user for their review. </p>
+      
+<div>
+
+  <p>Contact: Paul Smith</p>
+  <p>Credential: LAR for ACME Corp.</p>
+<br></br>
+<br />
+</div>
+      <div class="flex flex-justify-between">
+        <Button class="button--gray-dk button--big button--no-transform" raised label="Go Back" />
+        <Button
+          class="button--big button--no-transform"
+          raised
+          label="Submit"
+          onclick={() => {
+            this.step++;
+          }}
+        />
+      </div>
+    </>,
+
+<>
+<h3>Revocation Submitted!</h3>
         <p class="p-tag">
-          Once you are authorized to act on behalf of GLEIF, you perform identity assurance of a person serving in the
-          role of QAR. A GAR and the QAR will complete a real-time OOBI session in which the GAR and the QAR are
-          present.
-          <br />
-          <br />
-        </p>
-        <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button--big button--no-transform" raised label="Skip" />
-          <Button
-            class="button--big button--no-transform"
-            raised
-            label="Continue"
-            onclick={() => {
-              this.step++;
-            }}
-          />
-        </div>
-      </>,
-      <>
-        <h3>Grant Credentials</h3>
-        <img src={verifyCredentials} style={{ width: '60%', margin: '4rem 0 4rem 4rem' }} />
-        <p class="p-tag">
-          The GAR approves the issuance of the QVI vLEI Credential and the QVI receives the credential in its credential
-          wallet. The QVI vLEI Credential may be added or revoked at any time.
-          <br />
-          <br />
-        </p>
-        <div class="flex flex-justify-between">
-          <Button class="button--gray-dk button--big button--no-transform" raised label="Skip" />
-          <Button class="button--big button--no-transform" raised label="Close" onclick={vnode.attrs.end} />
-        </div>
-      </>,
+        You will be notified when the credential revocation is completed.</p>
+<div>
+
+<p>Contact: Paul Smith</p>
+<p>Credential: LAR for ACME Corp.</p>
+<br></br>
+<br />
+</div>
+<div class="flex flex-justify-end">
+  
+  <Button
+    class="button--big button--no-transform"
+    raised
+    label="Close"
+    onclick={vnode.attrs.end}
+  />
+</div>
+</>,
+      
     ];
   }
 
