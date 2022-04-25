e440ef7ffa3a809ebce8bda6552fa6cf15bbf214
Kevin Griffin <griffin.kev@gmail.com>
2022-04-25 08:39:40 -0700

    adds ward packaging

---
+++ app/index.js
@@ -0,0 +1,84 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+const {spawn} = require('child_process');
+const retry = require('promise-retry');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+let ward = null;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+    // eslint-disable-line global-require
+    app.quit();
+}
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        icon: `${__dirname}/assets/icon.icns`
+    });
+
+    // noinspection JSIgnoredPromiseFromCall
+    win.loadFile(__dirname + '/index.html');
+
+    win.webContents.openDevTools()
+
+    if (ward === null) {
+        ward = spawn(`${__dirname}/ward/ward`);
+        ward.stdout.on('data', (data) => {
+            let buffer = Buffer.from(data);
+            console.log('out:', buffer.toString());
+        });
+
+        ward.stderr.on('data', (data) => {
+            let buffer = Buffer.from(data);
+            let err = buffer.toString()
+            if (err.match(/Address already in use/) ||
+                err.match(/keri.kering.AuthError/ ||
+                err.match(/keri.kering.ConfigurationError/) )
+            ) {
+                // noinspection JSIgnoredPromiseFromCall
+                win.loadFile(__dirname + '/oops.html');
+                ward.kill();
+            }
+            console.log('err:', err);
+        });
+
+        ward.on('close', (code) => {
+            console.log(`ward process exited with code ${code}`);
+        });
+    }
+
+    retry((retry) => {
+        console.log('⏳ launching...');
+        return fetch('http://localhost:5623').catch(retry);
+    }).then(() => {
+        win.loadURL('http://localhost:5623').then(() =>
+            console.log('🚀 launched...'))
+    }).catch(() => {
+        ward.kill();
+        app.quit()
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('will-quit', () => {
+    ward.kill();
+});
