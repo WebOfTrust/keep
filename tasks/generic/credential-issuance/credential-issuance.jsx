66bc39043c27341b0e4f4ad8b8f61d9c58bd7ca9
jprice <jprice@izeni.com>
2022-05-12 15:07:19 -0600

    Start wiring up and fix styles for credential issuance

--- tasks/generic/credential-issuance/credential-issuance.jsx
+++ tasks/generic/credential-issuance/credential-issuance.jsx
@@ -1,175 +1,191 @@
 import m from 'mithril';
+import moment from 'moment';
 import { Button, Select, TextField } from '../../../src/app/components';
+import { KERI } from '../../../src/app/services';
 import approveRequest from '../../../src/assets/img/approve-request.svg';
 import githubLogo from '../../../src/assets/img/github-logo.svg';
 
-class IssueCreds {
-  constructor(vnode) {
+class CredentialIssuance {
+  constructor() {
+    this.currentState = 'issue-credential';
+    this.contacts = [];
+    this.credentials = [
+      {
+        label: 'QAR for QVI Corp.',
+        value: 'qarforqvicorp',
+      },
+    ];
     this.contact = null;
     this.credential = null;
   }
 
-  view(vnode) {
-    return (
-      <>
-        <h3>Issue Credentials</h3>
-        <p class="p-tag">Contact</p>
-        <TextField
-          outlined
-          fluid
-          style={{ marginBottom: '2rem' }}
-          iconTrailing={{
-            icon: 'search',
-          }}
-          value={this.contact}
-          oninput={(e) => {
-            this.contact = e.target.value;
-          }}
-        />
-        <p class="p-tag">Credentials</p>
-        <Select
-          outlined
-          fluid
-          style={{ marginBottom: '2rem' }}
-          options={[
-            { label: 'A', value: 'a' },
-            { label: 'B', value: 'b' },
-            { label: 'C', value: 'c' },
-          ]}
-          value={this.credential}
-          onchange={(e) => {
-            this.credential = e;
-          }}
-        />
-        <div class="flex flex-justify-between" style={{ marginTop: '4rem' }}>
-          <Button
-            raised
-            class="button--no-transform button--gray-dk button--big"
-            label="Go Back"
-            onclick={vnode.attrs.back}
-          />
-          <Button class="button--big button--no-transform" raised label="Submit" onclick={vnode.attrs.continue} />
-        </div>
-      </>
-    );
+  oninit() {
+    KERI.getContacts().then((contacts) => {
+      this.contacts = contacts;
+    });
   }
-}
 
-class ReviewCredential {
-  constructor(vnode) {}
+  get currentDate() {
+    const now = moment();
+    return now.format('M/D/YY');
+  }
+
+  get contactsSelect() {
+    return this.contacts.map((contact) => {
+      return {
+        label: contact.alias,
+        value: contact.id,
+      };
+    });
+  }
+
   view(vnode) {
     return (
       <>
-        <h3>Review Credential</h3>
-        <p class="p-tag">Please review credential prior to issuing and make sure it is correct.</p>
-
-        <div class="flex flex-justify-start" style={{ margin: '2rem 0 2rem 0' }}>
-          <div class="flex" style={{ marginRight: '4rem', alignItems: 'center' }}>
-            <img src={tempProfPic} style={{ width: '100px', height: '100px' }} />
-          </div>
-          <div>
-            <h3>Bob Smith</h3>
-            <p class="p-tag">QAR for VeriTech</p>
-            <p class="p-tag">Issuing on 12/7/21</p>
-          </div>
-        </div>
-        <p class="p-tag">Provenance Chain</p>
-        <div class="flex" style={{ flexDirection: 'column' }}>
-          <div class="flex" style={{ alignItems: 'center' }}>
-            <img
-              src={addNewContacts}
-              style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '1rem' }}
+        {this.currentState === 'issue-credential' && (
+          <>
+            <h3>Issue Credential</h3>
+            <p class="p-tag">Contact</p>
+            <TextField
+              outlined
+              fluid
+              style={{ marginBottom: '2rem' }}
+              iconTrailing={{
+                icon: 'search',
+              }}
+              value={this.contact}
+              oninput={(e) => {
+                this.contact = e.target.value;
+              }}
             />
-            <p class="p-tag">
-              <b>
-                <u>ACME Corp.</u>
-              </b>{' '}
-              granting QAR credentials to{' '}
-              <b>
-                <u>Bob Smith</u>
-              </b>{' '}
-              on 11/2/20
-            </p>
-          </div>
-          <div></div>
-          <div
-            style={{ height: '30px', borderLeft: '1px solid black', marginLeft: '1.5rem', marginTop: '-15px' }}
-          ></div>
-          <div class="flex">
-            <img src={tempProfPic} style={{ width: '50px', borderRadius: '50%', marginRight: '1rem' }} />
-            <p class="p-tag">
-              <b>
-                <u>QVI credential to be issued on 12/7/21</u>
-              </b>
-            </p>
-          </div>
-        </div>
-        <div class="flex flex-justify-between" style={{ marginTop: '4rem' }}>
-          <Button
-            class="button--gray-dk button--big button--no-transform"
-            raised
-            label="Go Back"
-            onclick={vnode.attrs.back}
-          />
-          <Button class="button--big button--no-transform" raised label="Continue" onclick={vnode.attrs.continue} />
-        </div>
-      </>
-    );
-  }
-}
-
-class CredsAreIssued {
-  constructor(vnode) {}
-  view(vnode) {
-    return (
-      <>
-        <img src={approveRequest} style={{ width: '50%', margin: '1.5rem 0 2rem 0' }} />
-        <h3>Credentials are Issued</h3>
-        <div class="flex flex-justify-between" style={{ alignItems: 'center' }}>
-          {/* <img src={githubLogo} style={{ width: '10%', height: '10%', margin: '0 0 0 0' }} /> */}
-          <p class="p-tag" style={{ fontSize: '120%' }}>
-            Issued 1:23pm on 12/7/21
-          </p>
-        </div>
-        <p class="p-tag">
-          The issuance of Bob Smith’s vLEI Credential is complete. The vLEI Credential is now in their credential
-          wallet.
-        </p>
-
-        <div class="flex flex-justify-end" style={{ marginTop: '4rem' }}>
-          <Button class="button--big button--no-transform" raised label="Close" onclick={vnode.attrs.end} />
-        </div>
-      </>
-    );
-  }
-}
-
-class CredentialIssuance {
-  constructor() {
-    this.currentState = 'issue-creds';
-  }
-  view(vnode) {
-    return (
-      <>
-        {this.currentState === 'issue-creds' && (
-          <IssueCreds
-            back={vnode.attrs.end}
-            continue={() => {
-              this.currentState = 'review-creds';
-            }}
-          />
+            <p class="p-tag">Credentials</p>
+            <Select
+              outlined
+              fluid
+              style={{ marginBottom: '2rem' }}
+              options={this.credentials}
+              value={this.credential}
+              onchange={(e) => {
+                this.credential = e;
+              }}
+            />
+            <div class="flex flex-justify-end" style={{ marginTop: '4rem' }}>
+              {/* <Button
+                raised
+                class="button--no-transform button--gray-dk button--big"
+                label="Go Back"
+                onclick={() => {
+                  vnode.attrs.end();
+                }}
+              /> */}
+              <Button
+                class="button--big button--no-transform"
+                disabled={!this.contact || !this.credential}
+                raised
+                label="Preview"
+                onclick={() => {
+                  this.currentState = 'review-credential';
+                }}
+              />
+            </div>
+          </>
         )}
-        {this.currentState === 'review-creds' && (
-          <ReviewCredential
-            back={() => {
-              this.currentState = 'issue-creds';
-            }}
-            continue={() => {
-              this.currentState = 'creds-issued';
-            }}
-          />
+        {this.currentState === 'review-credential' && (
+          <>
+            <h3 style={{ marginBottom: '2rem' }}>Review Credential</h3>
+            <p class="p-tag" style={{ fontSize: '20px' }}>
+              Please review credential prior to issuing and make sure it is correct.
+            </p>
+            <div class="flex flex-justify-start" style={{ margin: '3rem 0 3rem 0' }}>
+              <div class="flex" style={{ marginRight: '2rem', alignItems: 'center' }}>
+                <img src={githubLogo} style={{ width: '100px', height: '100px' }} />
+              </div>
+              <div>
+                <h3 style={{ fontSize: '20px', margin: '.5rem 0' }}>Bob Smith</h3>
+                {/* <p class="p-tag" style={{ margin: '.5rem 0' }}>
+                  QAR for VeriTech
+                </p> */}
+                <p class="p-tag" style={{ fontSize: '20px', margin: '.5rem 0' }}>
+                  Issuing on {this.currentDate}
+                </p>
+              </div>
+            </div>
+            <p class="p-tag" style={{ fontSize: '20px' }}>
+              Provenance Chain
+            </p>
+            <div class="flex" style={{ flexDirection: 'column' }}>
+              <div class="flex" style={{ alignItems: 'center' }}>
+                <div class="flex">
+                  <img
+                    src={githubLogo}
+                    style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '1rem' }}
+                  />
+                  <p class="p-tag">
+                    <b>
+                      <u>ACME Corp.</u>
+                    </b>{' '}
+                    granting QAR credentials to{' '}
+                    <b>
+                      <u>Bob Smith</u>
+                    </b>{' '}
+                    on 11/2/20
+                  </p>
+                </div>
+              </div>
+              <div></div>
+              <div
+                style={{ height: '30px', borderLeft: '1px solid black', marginLeft: '1.5rem', marginTop: '-15px' }}
+              ></div>
+              <div class="flex">
+                <img
+                  src={githubLogo}
+                  style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: '1rem' }}
+                />
+                <p class="p-tag">
+                  <b>
+                    <u>QVI credential to be issued on {this.currentDate}</u>
+                  </b>
+                </p>
+              </div>
+            </div>
+            <div class="flex flex-justify-between" style={{ marginTop: '4rem' }}>
+              <Button
+                class="button--gray-dk button--big button--no-transform"
+                raised
+                label="Go Back"
+                onclick={() => {
+                  this.currentState = 'issue-credential';
+                }}
+              />
+              <Button
+                class="button--big button--no-transform"
+                raised
+                label="Submit"
+                onclick={() => {
+                  this.currentState = 'credential-issued';
+                }}
+              />
+            </div>
+          </>
         )}
-        {this.currentState === 'creds-issued' && <CredsAreIssued end={vnode.attrs.end} />}
+        {this.currentState === 'credential-issued' && (
+          <>
+            <img src={approveRequest} style={{ width: '230px', margin: '1.5rem 0 2rem 0' }} />
+            <h3>Credential Issued</h3>
+            <div class="flex flex-justify-between" style={{ alignItems: 'center' }}>
+              <p class="p-tag" style={{ fontSize: '20px' }}>
+                Issued 1:23pm on 12/7/21
+              </p>
+            </div>
+            <p class="p-tag">
+              The issuance of Bob Smith's vLEI Credential is complete. The vLEI Credential is now in their credential
+              wallet.
+            </p>
+            <div class="flex flex-justify-end" style={{ marginTop: '4rem' }}>
+              <Button class="button--big button--no-transform" raised label="Close" onclick={vnode.attrs.end} />
+            </div>
+          </>
+        )}
       </>
     );
   }
