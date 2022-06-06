fc26e48199d0a44499df2ea5f7419a82453402b8
pfeairheller <pfeairheller@gmail.com>
2022-06-06 14:21:23 -0700

    new task for joining a video call

---
+++ tasks/generic/join-video-call/forms/send-challenge/send-challenge.jsx
@@ -0,0 +1,61 @@
+import m from 'mithril';
+import { Button, TextField } from '../../../../../src/app/components';
+import { KERI } from '../../../../../src/app/services';
+
+class SendChallengeForm {
+  constructor() {
+    this.challengeMessage = '';
+    this.copied = false;
+  }
+
+  copyMessage(vnode) {
+    navigator.clipboard.writeText(this.challengeMessage).then(
+      () => {
+        this.copied = true;
+        vnode.attrs.participants.updateWords(this.challengeMessage.split(' '));
+        m.redraw();
+      },
+      () => {
+        this.copied = false;
+        m.redraw();
+      }
+    );
+  }
+
+  oninit(vnode) {
+
+  }
+
+  view(vnode) {
+    return (
+      <>
+        <h4>Paste Challenge Message</h4>
+        <TextField
+          outlined
+          textarea
+          rows={2}
+          fluid
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.04)', margin: '0 0 0 0' }}
+          value={this.challengeMessage}
+          oninput={(e) => {
+              this.challengeMessage = e.target.value;
+          }}
+        />
+        <div class="flex flex-align-center flex-justify-between" style={{ margin: '1rem 0' }}>
+          <Button
+            raised
+            class="button--no-transform button--gray"
+            label="Copy"
+            iconLeading="content_copy"
+            onclick={(e) => {
+              this.copyMessage(vnode);
+            }}
+          />
+          <p class="font-color--green font-weight--medium">{this.copied ? 'Challenge message copied!' : <br />}</p>
+        </div>
+      </>
+    );
+  }
+}
+
+module.exports = SendChallengeForm;
--- tasks/qar/variables.js
+++ tasks/qar/variables.js
@@ -1,99 +1,127 @@
 import m from 'mithril';
-import { TextTooltip } from '../../src/app/components';
+import {TextTooltip} from '../../src/app/components';
 import addNewContacts from '../../src/assets/img/add-new-contacts.svg';
-import projectPlanning from '../../src/assets/img/project-planning.svg';
-import createIdentifier from '../../src/assets/img/create-identifier.svg';
-import secureMessaging from '../../src/assets/img/secure-messaging.svg';
-import uploadFile from '../../src/assets/img/upload-file.svg';
 import verifyCredentials from '../../src/assets/img/verify-credentials.svg';
-import declineRequest from '../../src/assets/img/decline-request.svg';
-import calendar from '../../src/assets/img/calendar.svg';
-import loanApproved from '../../src/assets/img/loan-approved.svg';
 
 const QarVariables = {
-  createYourAid: {
-    welcome: {
-      title: (
-        <>
-          Welcome to your <TextTooltip label={<u>QVI</u>}>Qualified vLEI Issuer</TextTooltip> Software
-        </>
-      ),
-      paragraph: (
-        <>
-          This software is designed to help you complete verification of authorized representatives on behalf of GLEIF
-          and also as a storage place for all of your credentials.
-          <br />
-          <br />
-          The first step will be to create your Delegated AID, then you will receive a short tutorial, You may skip the
-          tutorial by selecting the “skip” button.
-        </>
-      ),
+    aidToSend: "multi",
+    initiateLECredentialIssuance: {
+        steps: {
+            list: [
+                "Initiate a Video Call",
+                "Use an OOBI protocol to obtain the Legal Entity's AID",
+                "Use an OOBI protocol to share your Multi-Sig AID",
+                "Obtain and sign a Challenge Message",
+                "Generate and send a Challenge Message",
+                "LAR signs and returns Challenge Message",
+                "Issue the Legal Entity Credential to the Legal Entity AID."
+            ]
+        }
     },
-    creatingAID: {
-      title: (
-        <>
-          Creating your{' '}
-          <TextTooltip label={<u>QAR AID</u>}>
-            <u>QVI</u> is an acronym for Qualified vLEI Authorized Representative.
-            <br />
-            <u>AID</u> is your identifier for your QVI software.
-          </TextTooltip>
-        </>
-      ),
-      paragraph: (
-        <>
-          In order to provide authorization, you will first have to create your own AID within the software and GLEIF
-          will verify you as an authorized party to act on your QVI’s behalf.
-        </>
-      ),
+    joinLECredentialIssuance: {
+        steps: {
+            list: [
+                "Join a Video Call",
+                "Use an OOBI protocol to obtain the Legal Entity's AID.",
+                "Obtain the Challenge Message from the Lead.",
+                "LAR signs and returns Challenge Message.",
+                "Approve the Legal Entity Credential proposed by the Lead."
+            ]
+        },
+        oobiRecipient: "Legal Entity Multi-Sig AID"
     },
-    stepsToCreate: {
-      title: 'Steps to Create Your QAR AID',
+    createYourAid: {
+        welcome: {
+            title: (
+                <>
+                    Welcome to your <TextTooltip label={<u>QVI</u>}>Qualified vLEI Issuer</TextTooltip> Software
+                </>
+            ),
+            paragraph: (
+                <>
+                    This software is designed to help you complete verification of authorized representatives on behalf
+                    of GLEIF
+                    and also as a storage place for all of your credentials.
+                    <br/>
+                    <br/>
+                    The first step will be to create your Delegated AID, then you will receive a short tutorial, You may
+                    skip the
+                    tutorial by selecting the “skip” button.
+                </>
+            ),
+        },
+        creatingAID: {
+            title: (
+                <>
+                    Creating your{' '}
+                    <TextTooltip label={<u>QAR AID</u>}>
+                        <u>QVI</u> is an acronym for Qualified vLEI Authorized Representative.
+                        <br/>
+                        <u>AID</u> is your identifier for your QVI software.
+                    </TextTooltip>
+                </>
+            ),
+            paragraph: (
+                <>
+                    In order to provide authorization, you will first have to create your own AID within the software
+                    and GLEIF
+                    will verify you as an authorized party to act on your QVI’s behalf.
+                </>
+            ),
+        },
+        stepsToCreate: {
+            title: 'Steps to Create Your QAR AID',
+        },
+        createYourAlias: {
+            paragraph: (
+                <>
+                    The alias should be an easy to remember name for your AID as a Qualified Authorized Representative
+                    (e.g. My
+                    Qualified vLEI Authorized Representative Identifier).
+                </>
+            ),
+        },
     },
-    createYourAlias: {
-      paragraph: (
-        <>
-          The alias should be an easy to remember name for your AID as a Qualified Authorized Representative (e.g. My
-          Qualified vLEI Authorized Representative Identifier).
-        </>
-      ),
+    introToYourRole: {
+        main: {
+            title: 'Intro to the QAR Role',
+            paragraph: (
+                <>
+                    You have now created your identifier! Before you start the a real time OOBI session with the GLEIF
+                    Authorized
+                    Representative (GAR) and obtain your credentials, here is a brief introduction to some of the tasks
+                    you will
+                    be able to complete in your role.
+                </>
+            ),
+        },
+        steps: [
+            {
+                title: 'Complete Identity Assurance',
+                image: addNewContacts,
+                paragraph: (
+                    <>
+                        Once you are authorized to act on behalf of the QVI, you complete identity authentication with a
+                        GAR. A GAR
+                        and the QAR will complete a real-time OOBI session in which the GAR and the QAR are present to
+                        accomplish
+                        this.
+                    </>
+                ),
+            },
+            {
+                title: 'Grant Credentials',
+                image: verifyCredentials,
+                paragraph: (
+                    <>
+                        The GAR approves the issuance of the QVI vLEI Credential and the QVI receives the credential in
+                        its
+                        credential wallet. The QVI vLEI Credential may be added or revoked at any time.
+                    </>
+                ),
+            },
+        ],
     },
-  },
-  introToYourRole: {
-    main: {
-      title: 'Intro to the QAR Role',
-      paragraph: (
-        <>
-          You have now created your identifier! Before you start the a real time OOBI session with the GLEIF Authorized
-          Representative (GAR) and obtain your credentials, here is a brief introduction to some of the tasks you will
-          be able to complete in your role.
-        </>
-      ),
-    },
-    steps: [
-      {
-        title: 'Complete Identity Assurance',
-        image: addNewContacts,
-        paragraph: (
-          <>
-            Once you are authorized to act on behalf of the QVI, you complete identity authentication with a GAR. A GAR
-            and the QAR will complete a real-time OOBI session in which the GAR and the QAR are present to accomplish
-            this.
-          </>
-        ),
-      },
-      {
-        title: 'Grant Credentials',
-        image: verifyCredentials,
-        paragraph: (
-          <>
-            The GAR approves the issuance of the QVI vLEI Credential and the QVI receives the credential in its
-            credential wallet. The QVI vLEI Credential may be added or revoked at any time.
-          </>
-        ),
-      },
-    ],
-  },
 };
 
 module.exports = QarVariables;
