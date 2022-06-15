da9aec4886d36ac097754e2b390e0712aabdb382
Kevin Griffin <griffin.kev@gmail.com>
2022-06-15 15:38:04 -0400

    sucessfully signed and packaged

--- app/hooks.js
+++ app/hooks.js
@@ -2,29 +2,29 @@ const {notarize} = require('electron-notarize');
 const {signAsync} = require('@electron/osx-sign')
 
 module.exports = {
-    postPackage: () => {
-        console.log("pre make");
-        signAsync({
+    postPackage: async () => {
+        console.log("\nstart sign");
+        await signAsync({
             app: 'out/keep-darwin-arm64/keep.app/',
             entitlements: './entitlements.plist',
             hardenedRuntime: true,
             gatekeeperAssess: false,
             identity: 'Developer ID Application: Kevin Griffin (WHU7A4EQ74)'
-        }).then(function () {
-            console.log("done signing");
-            notarize({
-                appBundleId: "org.gleif.keep",
-                appPath: "out/keep-darwin-arm64/keep.app/",
-                appleId: process.env.APPLE_ID,
-                appleIdPassword: process.env.APPLE_ID_PASSWORD,
-            }).then(() => {
-                console.log("noterize done");
-            }).catch((err) => {
-                console.log("noterize err", err);
-            });
-        }).catch(function (err) {
-            // Handle the error
-            console.log("signing error", err)
+        }).then(() => {
+            console.log("\nsigning done");
+        }).catch((err) => {
+            console.log("\nnoterize err", err);
+        });
+        console.log("\nstart noterize");
+        await notarize({
+            appBundleId: "org.gleif.keep",
+            appPath: "out/keep-darwin-arm64/keep.app/",
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_ID_PASSWORD,
+        }).then(() => {
+            console.log("\nnoterize done");
+        }).catch((err) => {
+            console.log("\nnoterize err", err);
         });
     }
 };
