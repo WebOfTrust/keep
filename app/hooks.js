308d21f1b5f3bdf879d34ecedca9af8dd955c0bf
Kevin Griffin <griffin.kev@gmail.com>
2022-06-16 12:00:47 -0400

    hooks

--- app/hooks.js
+++ app/hooks.js
@@ -5,26 +5,27 @@ module.exports = {
     postPackage: async () => {
         console.log("\nstart sign");
         await signAsync({
-            app: 'out/keep-darwin-arm64/keep.app/',
-            entitlements: './entitlements.plist',
-            hardenedRuntime: false,
-            gatekeeperAssess: false,
+            app: 'out/keep-darwin-arm64/foo.app/',
+            entitlements: "./entitlements.plist",
+            'entitlements-inherit': "./entitlements.plist",
+            'signature-flags': "library",
+            'hardened-runtime': true,
             identity: 'Developer ID Application: Kevin Griffin (WHU7A4EQ74)'
         }).then(() => {
             console.log("\nsigning done");
         }).catch((err) => {
-            console.log("\nnoterize err", err);
+            console.log("\nnotarize err", err);
         });
-        console.log("\nstart noterize");
+        console.log("\nstart notarize");
         await notarize({
             appBundleId: "org.gleif.keep",
             appPath: "out/keep-darwin-arm64/keep.app/",
             appleId: process.env.APPLE_ID,
             appleIdPassword: process.env.APPLE_ID_PASSWORD,
         }).then(() => {
-            console.log("\nnoterize done");
+            console.log("\nnotarize done");
         }).catch((err) => {
-            console.log("\nnoterize err", err);
+            console.log("\nnotarize err", err);
         });
     }
 };
