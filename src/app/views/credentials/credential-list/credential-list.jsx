90fb21c652c5a29d9ed9495ec883522b34d6f368
Kevin Griffin <griffin.kev@gmail.com>
2022-06-23 17:56:21 -0400

    adds credential list updates, trimming challenge message

--- src/app/views/credentials/credential-list/credential-list.jsx
+++ src/app/views/credentials/credential-list/credential-list.jsx
@@ -3,14 +3,15 @@ import m from 'mithril';
 class CredentialList {
   // handleClick = () => {};
   view(vnode) {
+      console.log(vnode.attrs);
+      console.log(vnode.attrs.type);
     return (
       <>
         <div
-          class="flex flex-justify-between divider"
+          class="flex flex-justify-between row"
           style={{
             alignItems: 'center',
             margin: '.2rem 0 .2rem 0',
-            height: '40px',
             padding: '25px 0 25px 0',
             cursor: 'pointer',
           }}
@@ -18,9 +19,48 @@ class CredentialList {
             vnode.attrs.setCredential(vnode.attrs.credential);
           }}
         >
-          <div class="flex" style={{ alignItems: 'center' }}>
+          <div class="flex" style={{ alignItems: 'center'}}>
             <div class="flex flex-column">
-              <p style={{ margin: '0 0 0 1rem', fontSize: '90%' }}>{vnode.attrs.schema['title']}</p>
+              <div style={{ margin: '0 0 0 1rem', fontSize: '90%' }} >
+                  <div style={{ fontWeight: 'bold', color:'#6C7476', paddingBottom: '0.5rem' }}>{vnode.attrs.schema['title']}</div>
+                  {vnode.attrs.type === 'issued' && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>Issued from: {vnode.attrs.identifiers.get(vnode.attrs.credential['sad']['i']).name}</div>
+                      </>
+                  )}
+                  {vnode.attrs.type === 'issued' && vnode.attrs.credential['sad']['a'] !== undefined && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>Issued to: {vnode.attrs.contacts.get(vnode.attrs.credential['sad']['a']['i']).alias}</div>
+                      </>
+                  )}
+                  {vnode.attrs.type === 'received' && (
+                      <div style={{ paddingLeft: '0.5rem'}}>Issued to: {vnode.attrs.identifiers.get(vnode.attrs.credential['sad']['a']['i']).name}</div>
+                  )}
+                  {vnode.attrs.type === 'received' && vnode.attrs.credential['sad']['a'] !== undefined && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>Issued from: {vnode.attrs.contacts.get(vnode.attrs.credential['sad']['i']).alias}</div>
+                      </>
+                  )}
+                  {vnode.attrs.schema['title'] === 'Qualified vLEI Issuer Credential' && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>LEI: {vnode.attrs.credential['sad']['a']['LEI']}</div>
+                      </>
+                  )}
+                  {vnode.attrs.schema['title'] === 'Legal Entity Official Organizational Role vLEI Credential' && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>LEI: {vnode.attrs.credential['sad']['a']['LEI']}</div>
+                          <div style={{ paddingLeft: '0.5rem'}}>Role: {vnode.attrs.credential['sad']['a']['officialRole']}</div>
+                          <div style={{ paddingLeft: '0.5rem'}}>Name: {vnode.attrs.credential['sad']['a']['personLegalName']}</div>
+                      </>
+                  )}
+                  {vnode.attrs.schema['title'] === 'Legal Entity Engagement Context Role vLEI Credential' && (
+                      <>
+                          <div style={{ paddingLeft: '0.5rem'}}>LEI: {vnode.attrs.credential['sad']['a']['LEI']}</div>
+                          <div style={{ paddingLeft: '0.5rem'}}>Role: {vnode.attrs.credential['sad']['a']['engagementContextRole']}</div>
+                          <div style={{ paddingLeft: '0.5rem'}}>Name: {vnode.attrs.credential['sad']['a']['personLegalName']}</div>
+                      </>
+                  )}
+              </div>
             </div>
           </div>
         </div>
