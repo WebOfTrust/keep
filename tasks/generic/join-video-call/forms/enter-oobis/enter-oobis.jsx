38bb4c0537a0e25e9cda8ffd8dd33235dadaec0e
pfeairheller <pfeairheller@gmail.com>
2022-06-22 17:27:50 -0700

    Apply OOBI URL textarea change to Join Video Call OOBI Form.

--- tasks/generic/join-video-call/forms/enter-oobis/enter-oobis.jsx
+++ tasks/generic/join-video-call/forms/enter-oobis/enter-oobis.jsx
@@ -99,8 +99,7 @@ class EnterOOBIsForm {
                   {oobi.status === 'started' && <p className="font-color--blue font-weight--medium">In Progress</p>}
                   {oobi.status === 'resolved' && <p className="font-color--green font-weight--medium">Complete!</p>}
                 </div>
-                <div class="flex flex-align-center flex-justify-between">
-                  <h5 style={{ minWidth: '100px' }}>Alias:</h5>
+                <h5 style={{ margin: '1rem 0 .5rem' }}>Alias:</h5>
                   <TextField
                     outlined
                     fluid
@@ -110,19 +109,17 @@ class EnterOOBIsForm {
                       oobi.alias = e.target.value;
                     }}
                   />
-                </div>
-                <div class="flex flex-align-center flex-justify-between">
-                  <h5 style={{ minWidth: '100px' }}>URL:</h5>
+                <h5 style={{ margin: '1rem 0 .5rem' }}>OOBI URL:</h5>
                   <TextField
+                  textarea
                     outlined
                     fluid
-                    style={{ backgroundColor: 'rgba(0, 0, 0, 0.04)', height: '44px' }}
+                  style={{ backgroundColor: 'rgba(0, 0, 0, 0.04)' }}
                     value={oobi.url}
                     oninput={(e) => {
                       oobi.url = e.target.value;
                     }}
                   />
-                </div>
               </Card>
             );
           })}
