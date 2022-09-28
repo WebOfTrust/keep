32cd8702fdcd3c7a27ccd0c8423f700e86cd2452
jprice <jprice@izeni.com>
2022-09-28 09:33:06 -0600

    Remove text from navrail, make it a tooltip instead

--- src/app/components/nav-rail/nav-rail.jsx
+++ src/app/components/nav-rail/nav-rail.jsx
@@ -8,30 +8,33 @@ class NavRail {
         <div class="nav-rail">
           <div
             class={`nav-rail-item ${vnode.attrs.selected === 'dashboard' ? 'nav-rail-item--selected' : ''}`}
+            title="Dashboard"
             onclick={() => {
               m.route.set('/');
             }}
           >
-            <i class="material-icons md-48">dashboard</i>
-            <div class="nav-rail-item-text">Dashboard</div>
+            <i class="material-icons md-40">dashboard</i>
+            {/* <div class="nav-rail-item-text">Dashboard</div> */}
           </div>
           <div
             class={`nav-rail-item ${vnode.attrs.selected === 'contacts' ? 'nav-rail-item--selected' : ''}`}
+            title="My Contacts"
             onclick={() => {
               m.route.set('/contacts');
             }}
           >
-            <span class="material-icons md-48">people</span>
-            <div class="nav-rail-item-text">My Contacts</div>
+            <span class="material-icons md-40">people</span>
+            {/* <div class="nav-rail-item-text">My Contacts</div> */}
           </div>
           <div
             class={`nav-rail-item ${vnode.attrs.selected === 'credentials' ? 'nav-rail-item--selected' : ''}`}
+            title="My Credentials"
             onclick={() => {
               m.route.set('/credentials');
             }}
           >
-            <i class="material-icons md-48">lock</i>
-            <div class="nav-rail-item-text">My Credentials</div>
+            <i class="material-icons md-40">lock</i>
+            {/* <div class="nav-rail-item-text">My Credentials</div> */}
           </div>
         </div>
       </>
