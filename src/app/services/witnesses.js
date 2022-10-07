8982bb2542e1f3df1b51ceb8715507a0cdfef47f
Kevin Griffin <griffin.kev@gmail.com>
2022-10-07 11:36:03 -0400

    Feat/staging (#227)
    * adds GLEIF staging witness network
    
    * typos
    
    * fixes typos
    
    * fixes keep name

--- src/app/services/witnesses.js
+++ src/app/services/witnesses.js
@@ -12,6 +12,10 @@ class Witnesses {
       label: 'Local Witness Pool',
       value: 'local',
     },
+    {
+      label: 'Staging Witness Pool',
+      value: 'staging',
+    },
   ];
 
   static witnesses = {
@@ -26,6 +30,12 @@ class Witnesses {
       'Boq71an-vhU6DtlZzzJF7yIqbQxb56rcxeB0LppxeDOA',
       'BHGK9Gem8PdiZ7PZ9WcIwxM7YnGaztYA65X3o5_RxFa8',
     ],
+    staging: [
+        'BILZrnru0e-0MUmnnjOdWTrZ7OW3sCuk_C_67uYeLsN_',
+        'BN6TBUuiDY_m87govmYhQ2ryYP2opJROqjDkZToxuxS2',
+        'BBD748wGFn9-1nfVEtfwd97wc-HCw0LRF2xeIujmqJOQ',
+        'BHeD7WvSDGwm0glBHGTuHpGeMRq7HyCOAJ8h_epQyHkR',
+    ]
   };
 
   static poolName(pool) {
